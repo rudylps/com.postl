fa6941a803ee5ca08e7e5e901156f01cc3541f7d
rudylps <rudy.pellard.lps@gmail.com>
2018-08-10 11:09:15 +0200

    maj back

--- src/main/java/com/postl/controller/LogementController.java
+++ src/main/java/com/postl/controller/LogementController.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,14 +15,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.ResourceAccessException;
 
 import com.postl.model.Logement;
-import com.postl.model.Utilisateur;
 import com.postl.repository.ILogementRepository;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("")
 public class LogementController {
 
@@ -38,11 +39,21 @@ public class LogementController {
 	public Optional<Logement> findOneLogement(@PathVariable(value = "logement_id") Long logement_id) {
 		return iLogementRepository.findById(logement_id);
 	}
+	
+	@GetMapping("/chercherLogement")
+	public List<Logement> chercherLogement(
+			@RequestParam(name="ville", defaultValue="") String ville,
+			@RequestParam(name="cp", defaultValue="") String cp
+			) {
+		return iLogementRepository.chercher("%" + ville + "%", "%" + cp + "%");
+	}
 
 	@PutMapping("/logement/{logement_id}")
 	public Logement updateLogement(@PathVariable Long logement_id,
 			@Valid @RequestBody Logement logementDetails) {
+		
 		return iLogementRepository.findById(logement_id).map(logement -> {
+			
 			logement.setPhoto(logementDetails.getPhoto());
 			logement.setNumero_rue(logementDetails.getNumero_rue());
 			logement.setType_rue(logementDetails.getType_rue());
@@ -53,6 +64,7 @@ public class LogementController {
 			logement.setPrix_nuit(logementDetails.getPrix_nuit());
 			logement.setDescription(logementDetails.getDescription());
 			return iLogementRepository.save(logement);
+			
 		}).orElseThrow(() -> new ResourceAccessException(logement_id + " ??"));
 	}
 
